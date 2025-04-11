a0462716db75c04462be588cf018de7fc6257031
Daniël van Eeden <git@myname.nl>
2025-04-11 09:21:40 +0200

    Update

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log/slog"
+	"sort"
 
 	"github.com/go-mysql-org/go-mysql/mysql"
 	"github.com/go-mysql-org/go-mysql/replication"
@@ -16,7 +16,6 @@ func processEvent(e *replication.BinlogEvent) error {
 	if g, ok := e.Event.(*replication.GTIDEvent); ok {
 		u, _ := uuid.FromBytes(g.SID)
 		gtid := fmt.Sprintf("%s:%d", u.String(), g.GNO)
-		slog.Debug("processing gtid", "gtid", gtid)
 
 		var err error
 		if gSet == nil || gSet.IsEmpty() {
@@ -36,15 +35,31 @@ func main() {
 
 	p := replication.NewBinlogParser()
 
+	gSets := make(map[string]mysql.GTIDSet, 0)
+
 	for _, file := range flag.Args() {
-		slog.Info("parsing file", "filename", file)
 		err := p.ParseFile(file, 4, processEvent)
 		if err != nil {
 			println(err.Error())
 		}
-		if gSet != nil {
-			fmt.Println(gSet.String())
-			gSet = nil
+		gSets[file] = gSet
+		gSet = nil
+	}
+
+	binlogs := make([]string, len(gSets))
+	for f := range gSets {
+		binlogs = append(binlogs, f)
+	}
+	sort.Strings(binlogs)
+
+	for _, f := range binlogs {
+		if f == "" {
+			continue
+		}
+		if gSets[f] == nil {
+			fmt.Printf("%s\t%s\n", f, "<empty>")
+		} else {
+			fmt.Printf("%s\t%s\n", f, gSets[f].String())
 		}
 	}
 }
